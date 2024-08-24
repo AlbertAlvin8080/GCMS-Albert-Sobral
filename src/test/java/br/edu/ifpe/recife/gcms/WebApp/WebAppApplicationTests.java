b6b4aca40cbae4e003194e99ce9c400240c55050
Albert Alvin <121321650+albertalvin8080@users.noreply.github.com>
2024-08-23 21:23:15 -0300

    Atualizar o WebAppApplicationTests.java

--- src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
+++ src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
@@ -48,6 +48,6 @@ class WebAppApplicationTests {
         this.mockMvc.perform(get("/main"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, ")));
+                .andExpect(content().string(containsString("Hello, jshshsidb")));
     }
 }
